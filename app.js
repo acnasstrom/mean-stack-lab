ae3701e3cda49a86b3c10bd4907553fd01c8a8a2
Ann-Cathrin Näsström <ann-cathrin.nasstrom@valtech.se>
2015-02-03 15:44:33 +0100

    Use Angular routing, states and templates.

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-angular.module("flapperNews", [])
+angular
+  .module("flapperNews", ["ui.router"])
 
-  .factory("posts", [ function() {
-    var o = {
-      posts: []
-    };
-    return o;
-  }])
+  .config([
+    "$stateProvider",
+    "$urlRouterProvider",
 
-  .controller("MainCtrl", [ "$scope", "posts", function($scope, posts) {
-    $scope.posts = posts.posts;
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider.state("home", { url: "/home", templateUrl: "/home.html", controller: "MainCtrl" });
+      $urlRouterProvider.otherwise("home")
+    }
+  ])
 
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title == "") { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = "";
-      $scope.link = "";
-    };
+  .factory("posts", [
+    function() {
+      var o = {
+        posts: []
+      };
+      return o;
+    }
+  ])
 
-    $scope.incrementUpvotes = function(post){
-      post.upvotes += 1;
-    };
-  }]);
+  .controller("MainCtrl", [
+    "$scope",
+    "posts",
+
+    function($scope, posts) {
+      $scope.posts = posts.posts;
+
+      $scope.addPost = function(){
+        if(!$scope.title || $scope.title == "") { return; }
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0
+        });
+        $scope.title = "";
+        $scope.link = "";
+      };
+
+      $scope.incrementUpvotes = function(post){
+        post.upvotes += 1;
+      };
+    }
+  ]);
