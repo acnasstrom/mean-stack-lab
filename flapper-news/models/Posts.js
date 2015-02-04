ccb59fde74f6c049193d7363b39718055ccb4ffa
Ann-Cathrin Näsström <ann-cathrin.nasstrom@valtech.se>
2015-02-04 14:58:56 +0100

    Return Comments along with a Post.
    I also figured something out: The tutorial models Comments in a separate collection instead of embedded documents on Post to be able to upvote a single comment. Actions on a single embedded document are usually somewhat cumbersome.

--- flapper-news/models/Posts.js
+++ flapper-news/models/Posts.js
@@ -4,8 +4,6 @@ var PostSchema = new mongoose.Schema({
   title: String,
   link: String,
   upvotes: { type: Number, default: 0 },
-  // Hm, I would have used an embedded document for Comments instead...
-  // Maybe the point of keeping Comments in a separate collection will be revealed later in the tutorial.
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -41,7 +41,11 @@ router.param('post', function(req, res, next, id) {
 });
 
 router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+  req.post.populate('comments', function(err, post) {
+    if(err) { return next(err); }
+
+    res.json(req.post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
